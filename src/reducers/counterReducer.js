4a682c90449ba1e3257d6f46e4b6b98f1fefb025
deepakmahapatra047 <107745111+deepakmahapatra047@users.noreply.github.com>
2022-12-08 15:51:17 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,24 +1,10 @@
+const counterReducer = (state, action) => {
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  }
+  if (action.type === "DECREMENT") {
+    return state - 1;
+  }
+};
 
-   
-
-const counterReducer = (state,action) =>{
-
-
-    const initialState = 0;
-switch(action){
-
-    case "increment" :
-        return state +1;
-    case "decrement" :
-        return state-1;
-    case "reset":
-        return initialState;
-    default :
-    return state;
-
-
-}
-   
-}
-
-export {counterReducer}
+export { counterReducer };
